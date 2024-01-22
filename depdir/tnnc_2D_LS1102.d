7d1b850c26a623bfce3b818c963e68d77bf4ee23
dhike1 <157415341+dhike1@users.noreply.github.com>
2024-01-22 14:57:26 -0500

    Add files via upload

---
+++ depdir/tnnc_2D_LS1102.d
@@ -0,0 +1,208 @@
+tnnc_2D_LS1102.o: \
+ /opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/tnnc_2D_LS1102.ovl \
+ /usr/include/stdc-predef.h /opt/PV-360.3.3/prog/include/mr/recotyp.h \
+ /opt/PV-360.3.3/prog/include/imagetyp.h \
+ /opt/PV-360.3.3/prog/include/common/recotyp.h \
+ /opt/PV-360.3.3/prog/include/libRecoTools/RecoStageTyp.h \
+ /opt/PV-360.3.3/prog/include/subject/subjtyp.h \
+ /opt/PV-360.3.3/prog/include/generated/ParxDefs.h \
+ /opt/PV-360.3.3/prog/include/mr/ta_config.h \
+ /opt/PV-360.3.3/prog/include/Parx/Dynenum.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/mrTypes.h \
+ /opt/PV-360.3.3/prog/include/common/PvTypes/commonTypes.h \
+ /opt/PV-360.3.3/prog/include/PvUtil/DataPathType.h \
+ /opt/PV-360.3.3/prog/include/generated/DataPath.h \
+ /opt/PV-360.3.3/prog/include/generated/KeyUids.h \
+ /opt/PV-360.3.3/prog/include/PvAdj/adjConfigTypes.h \
+ /opt/PV-360.3.3/prog/include/PvAdj/adjBaseTypes.h \
+ /opt/PV-360.3.3/prog/include/PvAdj/adjManagerTypes.h \
+ /opt/PV-360.3.3/prog/include/PvUtil/TimeDefs.h \
+ /opt/PV-360.3.3/prog/include/PvAdj/ResultParsType.h \
+ /opt/PV-360.3.3/prog/include/modality.h \
+ /opt/PV-360.3.3/prog/include/common/PvTypes/geoTypes.h \
+ /opt/PV-360.3.3/prog/include/pvidl.h \
+ /opt/PV-360.3.3/prog/include/bruktyp.h \
+ /opt/PV-360.3.3/prog/include/generated/GeoObjIds.h \
+ /opt/PV-360.3.3/prog/include/common/PvTypes/reportTypes.h \
+ /opt/PV-360.3.3/prog/include/generated/ParImport.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/pvmTypes.h \
+ /opt/PV-360.3.3/prog/include/configtyp.h \
+ /opt/PV-360.3.3/prog/include/PvMrAdj/MrAdjManagerTypes.h \
+ /opt/PV-360.3.3/prog/include/PvAdj/adjBaseTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/acqtyp.h \
+ /opt/PV-360.3.3/prog/include/common/acqtyp.h \
+ /opt/PV-360.3.3/prog/include/acqutyp.h \
+ /opt/PV-360.3.3/prog/include/gradient_ramp_typ.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/epiTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/spiralTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/trajectoryTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/wobbleTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/internalPvmTypes.h \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/spiralTypesInternal.h \
+ /opt/PV-360.3.3/prog/include/mr/proctyp.h \
+ /opt/PV-360.3.3/prog/include/Visu/Mr/VisuTypes.h \
+ /opt/PV-360.3.3/prog/include/Visu/Mxi/VisuTypes.h \
+ /opt/PV-360.3.3/prog/include/Visu/Common/VisuTypes.h \
+ /opt/PV-360.3.3/prog/include/common/PvAdminRoute.h \
+ /opt/PV-360.3.3/prog/include/Visu/Common/VisuDefines.h \
+ /opt/PV-360.3.3/prog/include/proto/cmn_baselevel_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/cmn_subject_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/cmn_config_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/cmn_pvm_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/cmn_visu_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/mr_baselevel_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/mr_pvm_extern.def \
+ /opt/PV-360.3.3/prog/include/proto/mr_visu_extern.def \
+ /opt/PV-360.3.3/prog/include/mr/PvTypes/method.fmt \
+ /opt/PV-360.3.3/prog/include/common/PvTypes/method.fmt \
+ /opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsTypes.h \
+ /opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsDefinition.h \
+ /opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/callbackDefs.h \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/method.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/spectro.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/epi.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/epg_vfl.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/spiral.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/trajectory.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/diffusion.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/startupShim.grp \
+ /opt/PV-360.3.3/prog/include/mr/PvGroups/modules.grp \
+ /opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsLayout.h
+
+/usr/include/stdc-predef.h:
+
+/opt/PV-360.3.3/prog/include/mr/recotyp.h:
+
+/opt/PV-360.3.3/prog/include/imagetyp.h:
+
+/opt/PV-360.3.3/prog/include/common/recotyp.h:
+
+/opt/PV-360.3.3/prog/include/libRecoTools/RecoStageTyp.h:
+
+/opt/PV-360.3.3/prog/include/subject/subjtyp.h:
+
+/opt/PV-360.3.3/prog/include/generated/ParxDefs.h:
+
+/opt/PV-360.3.3/prog/include/mr/ta_config.h:
+
+/opt/PV-360.3.3/prog/include/Parx/Dynenum.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/mrTypes.h:
+
+/opt/PV-360.3.3/prog/include/common/PvTypes/commonTypes.h:
+
+/opt/PV-360.3.3/prog/include/PvUtil/DataPathType.h:
+
+/opt/PV-360.3.3/prog/include/generated/DataPath.h:
+
+/opt/PV-360.3.3/prog/include/generated/KeyUids.h:
+
+/opt/PV-360.3.3/prog/include/PvAdj/adjConfigTypes.h:
+
+/opt/PV-360.3.3/prog/include/PvAdj/adjBaseTypes.h:
+
+/opt/PV-360.3.3/prog/include/PvAdj/adjManagerTypes.h:
+
+/opt/PV-360.3.3/prog/include/PvUtil/TimeDefs.h:
+
+/opt/PV-360.3.3/prog/include/PvAdj/ResultParsType.h:
+
+/opt/PV-360.3.3/prog/include/modality.h:
+
+/opt/PV-360.3.3/prog/include/common/PvTypes/geoTypes.h:
+
+/opt/PV-360.3.3/prog/include/pvidl.h:
+
+/opt/PV-360.3.3/prog/include/bruktyp.h:
+
+/opt/PV-360.3.3/prog/include/generated/GeoObjIds.h:
+
+/opt/PV-360.3.3/prog/include/common/PvTypes/reportTypes.h:
+
+/opt/PV-360.3.3/prog/include/generated/ParImport.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/pvmTypes.h:
+
+/opt/PV-360.3.3/prog/include/configtyp.h:
+
+/opt/PV-360.3.3/prog/include/PvMrAdj/MrAdjManagerTypes.h:
+
+/opt/PV-360.3.3/prog/include/PvAdj/adjBaseTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/acqtyp.h:
+
+/opt/PV-360.3.3/prog/include/common/acqtyp.h:
+
+/opt/PV-360.3.3/prog/include/acqutyp.h:
+
+/opt/PV-360.3.3/prog/include/gradient_ramp_typ.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/epiTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/spiralTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/trajectoryTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/wobbleTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/internalPvmTypes.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/spiralTypesInternal.h:
+
+/opt/PV-360.3.3/prog/include/mr/proctyp.h:
+
+/opt/PV-360.3.3/prog/include/Visu/Mr/VisuTypes.h:
+
+/opt/PV-360.3.3/prog/include/Visu/Mxi/VisuTypes.h:
+
+/opt/PV-360.3.3/prog/include/Visu/Common/VisuTypes.h:
+
+/opt/PV-360.3.3/prog/include/common/PvAdminRoute.h:
+
+/opt/PV-360.3.3/prog/include/Visu/Common/VisuDefines.h:
+
+/opt/PV-360.3.3/prog/include/proto/cmn_baselevel_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/cmn_subject_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/cmn_config_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/cmn_pvm_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/cmn_visu_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/mr_baselevel_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/mr_pvm_extern.def:
+
+/opt/PV-360.3.3/prog/include/proto/mr_visu_extern.def:
+
+/opt/PV-360.3.3/prog/include/mr/PvTypes/method.fmt:
+
+/opt/PV-360.3.3/prog/include/common/PvTypes/method.fmt:
+
+/opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsTypes.h:
+
+/opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsDefinition.h:
+
+/opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/callbackDefs.h:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/method.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/spectro.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/epi.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/epg_vfl.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/spiral.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/trajectory.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/diffusion.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/startupShim.grp:
+
+/opt/PV-360.3.3/prog/include/mr/PvGroups/modules.grp:
+
+/opt/PV-360.3.3/prog/curdir/scc89/ParaVision/methods/src/tnnc_2D_LS1102/parsLayout.h:
